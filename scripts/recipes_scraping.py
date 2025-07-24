63814dffab2d6a4249a3fe464ba3ee877fa6fc42
Raoul Normand <raoul.normand@gmail.com>
2025-07-24 12:43:44 -0400

    Add scripts for data scraping.

---
+++ scripts/recipes_scraping.py
@@ -0,0 +1,53 @@
+"""
+Scrapes recipes from recipetineats.com, using the list of urls
+of the recipes_urls.txt file.
+"""
+
+# Imports
+
+import time
+from random import randint
+
+from src.scraping import soupify
+
+# Functions
+
+
+def get_instructions(url):
+    """
+    Gets the recipe instructions on the given page.
+    """
+    recipe = soupify(url)
+    # Instructions are listed with the class below
+    instructions_list = recipe.find_all("div", class_="wprm-recipe-instruction-text")
+    # Get only the text and concatenate
+    instructions_text = [instruction.get_text() for instruction in instructions_list]
+    return " ".join(instructions_text)
+
+
+# Main function
+
+
+def main():
+    """
+    Adds the instructions all recipe links and instructions
+    to a csv file.
+    """
+    # Save instructions from each url in a csv file
+    with open(
+        "./results/recipes_instructions.csv", "w", encoding="utf-8"
+    ) as recipes_instructions:
+        recipes_instructions.write("url, instructions\n")
+        with open("./results/recipes_urls.txt", "r") as recipes_urls:
+            for url in recipes_urls:
+                instructions = get_instructions(url)
+                # Only add if there are instructions, i.e. the link is to a recipe
+                if instructions:
+                    # Replace , by ; to avoid issues in csv
+                    instructions = instructions.replace(",", ";")
+                    recipes_instructions.write(url.strip() + ", " + instructions + "\n")
+                time.sleep(randint(1, 3))
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/urls_scraping.py
@@ -0,0 +1,46 @@
+"""
+Scrapes recipe urls from recipetineats.com and saves to recipes_list.txt.
+The list of all recipes is at the url
+https://www.recipetineats.com/recipes/?fwp_paged=*
+with * from 1 to 80 (as of July 2025)
+"""
+
+# Imports
+
+import time
+from random import randint
+from src.scraping import soupify
+
+# Constants
+
+NUMBER_RECIPE_PAGES = 80
+
+# Functions
+
+def get_recipe_links(url):
+    """
+    Gets all the recipe links on the given page.
+    """
+    soup = soupify(url)
+    # Recipes are listed with the class below
+    recipes = soup.find_all("a", class_="entry-title-link")
+    # Extract the html from the list
+    return [recipe["href"] for recipe in recipes]
+
+# Main function
+
+def main():
+    """
+    Adds all recipe links to a txt file.
+    """
+    for nb_page in range(1, NUMBER_RECIPE_PAGES):
+        url = "https://www.recipetineats.com/recipes/?fwp_paged=" + str(nb_page)
+        recipes_urls = get_recipe_links(url)
+        with open("./results/recipes_urls.txt", "a") as f:
+            for line in recipes_urls:
+                f.write(f"{line}\n")
+        time.sleep(randint(0, 3))
+
+
+if __name__ == "__main__":
+    main()
